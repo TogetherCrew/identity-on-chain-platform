8692fcc35951dcea190ac8847869376b2669c607
mehditorabiv <mehditorabiv@gmail.com>
2024-07-10 01:28:26 +0300

    install roboto font family

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,11 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
