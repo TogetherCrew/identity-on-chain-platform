db572d43e76a3f5e15cccb2808a112e697e87f0c
mehditorabiv <mehditorabiv@gmail.com>
2024-07-11 01:52:53 +0300

    update readme

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,22 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: ["./src/setupTests.ts"],
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
     coverage: {
-      provider: "istanbul",
-      reporter: ["text", "lcov"],
+      provider: 'istanbul',
+      reporter: ['text', 'lcov'],
     },
   },
   server: {
     port: 3000,
-  },
-  resolve: {
-    alias: { "@": path.resolve(__dirname, "/src") },
   },
 });
