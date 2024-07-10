80980079dc73ec7d92f7e542fe96972a778611f6
mehditorabiv <mehditorabiv@gmail.com>
2024-07-11 01:33:37 +0300

    add test

---
+++ src/pages/Dashboard/Dashboard.spec.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Dashboard } from './Dashboard';
+
+test('renders Dashboard text', () => {
+  render(<Dashboard />);
+  const dashboardElement = screen.getByText(/dashboard/i);
+  expect(dashboardElement).toBeInTheDocument();
+});
