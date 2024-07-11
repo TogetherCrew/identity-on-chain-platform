2c9013fd25e688dd1d6088634922c32bde375713
mehditorabiv <mehditorabiv@gmail.com>
2024-07-11 11:02:40 +0300

    update interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,8 +1,11 @@
-import { ReactNode } from 'react';
+import { SvgIconTypeMap } from '@mui/material';
+import { OverridableComponent } from '@mui/material/OverridableComponent';
 
 export interface MenuItem {
   title: string;
   path: string;
-  icon?: ReactNode | JSX.Element;
+  icon: OverridableComponent<SvgIconTypeMap<NonNullable<unknown>, 'svg'>> & {
+    muiName: string;
+  };
   children?: MenuItem[];
 }
