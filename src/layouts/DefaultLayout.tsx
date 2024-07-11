8e5294a39f1f29abc8f362af649b2725f675b7a5
mehditorabiv <mehditorabiv@gmail.com>
2024-07-11 11:07:54 +0300

    fix typo

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { AppBar, Box } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 import SidebarApp from '../components/layouts/SidebarApp';
 
@@ -14,12 +14,14 @@ function DefaultLayout() {
           overflowX: 'hidden',
         }}
       >
+        <AppBar>ds</AppBar>
         <Box
           component="main"
           sx={{
             flexGrow: 1,
             px: 3,
             py: 2,
+            mt: 2,
             backgroundColor: (theme) => theme.palette.grey[50],
           }}
         >
