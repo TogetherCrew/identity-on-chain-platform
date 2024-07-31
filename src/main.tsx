b6586b9d15ff7bea278a15a150b33f3ba0bfca47
mehditorabiv <mehditorabiv@gmail.com>
2024-07-31 14:26:33 +0300

    install rainbowkit,view, wagmi

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import theme from "./libs/theme";
+import theme from './libs/theme';
 
 const queryClient = new QueryClient({
   defaultOptions: {
