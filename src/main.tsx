d45732acda069985b3e79ad434267c0ef3580c69
mehditorabiv <mehditorabiv@gmail.com>
2024-08-30 13:11:58 +0300

    undo changes

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/identity-on-chain-platform.pages/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/identity-on-chain-platform.pages/',
   plugins: [react(), nodePolyfills()],
   test: {
     globals: true,
