0a36f0828e7d23bb78c1f8b0a3ed098e9cc89691
mehditorabiv <mehditorabiv@gmail.com>
2024-08-30 12:45:31 +0300

    add base

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/identity-on-chain-platform/',
   plugins: [react(), nodePolyfills()],
   test: {
     globals: true,
