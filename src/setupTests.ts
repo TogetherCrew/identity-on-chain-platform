b284d8f42eaf79c7cedaa312e07af5d56e3bb9ca
mehditorabiv <mehditorabiv@gmail.com>
2024-09-01 22:58:45 +0300

    fix

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,4 +3,6 @@ import '@testing-library/jest-dom';
 import * as matchers from '@testing-library/jest-dom/matchers';
 import { expect } from 'vitest';
 
+process.env.VITE_API_BASE_URL = 'https://onchain.togethercrew.de/api/v1/';
+
 expect.extend(matchers);
