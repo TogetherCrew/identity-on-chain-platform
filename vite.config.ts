14bd16027ee578080d85500ffd2079217e4c2773
mehditorabiv <mehditorabiv@gmail.com>
2025-01-16 15:44:08 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,6 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
   test: {
     globals: true,
     environment: 'jsdom',
